e8dd38abe9d28e7bd8de03358af627b30437df10
raghavendrareddy23 <raghavendrareddy.munagala23@gmail.com>
2024-08-27 18:45:28 +0530

    Change in Api

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-mern-backend-mtnf.onrender.com",
+  baseURL: "https://ecommerce-mern-backend-a83s.onrender.com",
   // baseURL:"http://localhost:5000"
 });
 export default api;
